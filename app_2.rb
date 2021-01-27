a2bbf1f731075be6ae45b0d4bf8a114d9922ff03
floup42 <pousseurdec@gmail.com>
2021-01-27 02:21:55 +0100

    HOPE

--- app_2.rb
+++ app_2.rb
@@ -15,7 +15,7 @@ user = HumanPlayer.new(y)
 player1 = Player.new("Josiane")
 player2 = Player.new("José")
 
-
+while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
   puts "Quelle action veux-tu effectuer ?"
   puts ""
   puts "a - chercher une meilleure arme"
@@ -27,15 +27,17 @@ player2 = Player.new("José")
 
   menu = gets.chomp
 
-  if menu == 's'
+  if menu == "s"
     user.search_health_pack
   end
-  if menu.to_i == 0
+  if menu == "0"
     user.attacks(player1)
   end
-  if menu.to_i == 1
+  if menu == "1"
     user.attacks(player2)
   end
-  if menu == 'a'
+  if menu == "a"
     user.search_weapon
   end
+end
+puts "BRAVO !"
